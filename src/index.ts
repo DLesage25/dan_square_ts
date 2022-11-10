eaf5a4ba5dc1234b40ddd4cfc0d530caaad6fd76
Daniel Le'Sage <daniel@partnerhero.com>
2022-11-10 13:43:05 -0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
+/**
+ * Dan^2 TS playground
+ *
+ * - variable declaration with primitive types
+ * - variable declaration with complex/structured types
+ * - function declaration - param and return typing
+ * - type casting
+ * - dynamic typing
+ *
+ * - ts with npm
+ * - module typing with axios
+ *   - async/await
+ *   - template strings
+ */
+
 function index() {
     console.log("woot");
 }
